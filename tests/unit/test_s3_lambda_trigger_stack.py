e6fb57ebc88d11a3942727c145a44f85138df2ea
idrissa-mgs <idrissamagassa@gmail.com>
2021-11-27 21:46:06 +0100

    Initial commit

---
+++ tests/unit/test_s3_lambda_trigger_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from s3_lambda_trigger.s3_lambda_trigger_stack import S3LambdaTriggerStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in s3_lambda_trigger/s3_lambda_trigger_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = S3LambdaTriggerStack(app, "s3-lambda-trigger")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
