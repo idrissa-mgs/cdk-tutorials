006e52df9bee7faadd77466392a142241789954d
idrissa-mgs <idrissamagassa@gmail.com>
2021-11-28 16:24:53 +0100

    create my etl app

---
+++ lambda_/main.py
@@ -0,0 +1,17 @@
+import boto3
+import os
+
+
+s3 = boto3.client('s3')
+
+def handler(event, context):
+    bucket_name = (os.environ['BUCKET_NAME'])
+    key = event['Records'][0]['s3']['object']['key']    
+    try:        # Log the event
+        print("[LambdaListenet] New file with name {} created in bucket {}".format(key, bucket_name))  
+        response = {'status': 'success', 'key': key}
+        return response    
+    except Exception as e:
+        print(e)
+        print("[Error] :: Error processing file {} from bucket {}.   ".format(key, bucket_name))
+        raise e
--- s3_lambda_trigger/s3_lambda_trigger_stack.py
+++ s3_lambda_trigger/s3_lambda_trigger_stack.py
@@ -1,6 +1,10 @@
 from aws_cdk import (
-    core as cdk
-    # aws_sqs as sqs,
+    core as cdk,
+    aws_lambda as _lambda,
+    aws_iam  as _iam, 
+    aws_s3 as _s3,
+    aws_s3_notifications as _s3_notifications
+
 )
 
 # For consistency with other languages, `cdk` is the preferred import name for
@@ -15,10 +19,29 @@ class S3LambdaTriggerStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+
+
         # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "S3LambdaTriggerQueue",
-        #     visibility_timeout=cdk.Duration.seconds(300),
-        # )
+        #IAM USER
+        #user = _iam.User(self, "VSUser")
+
+        #S3BUCKET and grant 
+        bucket = _s3.Bucket(self, 'vs-bucket')
+        #bucket.grant_read_write(user)
+
+        #LAMBDA FUNCTION
+        my_function = _lambda.Function(self, "ma_funct_lambda", 
+        runtime=_lambda.Runtime.PYTHON_3_7,
+        code=_lambda.Code.from_asset('lambda_'),
+        handler="main.handler", 
+        environment={'BUCKET_NAME':bucket.bucket_name}
+        )
+
+        #S3 NOTIFICATION
+        notification = _s3_notifications.LambdaDestination(my_function)
+        notification.bind(self, bucket)
+
+        #add object creation notificaton
+
+        bucket.add_object_created_notification(notification)
